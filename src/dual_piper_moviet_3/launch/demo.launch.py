217235e7e3d744e7ca9c69571c4f7172bd4f08ba
forgetwhatuwant <forgetwhatuwant@gmail.com>
2025-04-10 17:26:29 +0800

    update for isaac

--- src/dual_piper_moviet_3/launch/demo.launch.py
+++ src/dual_piper_moviet_3/launch/demo.launch.py
@@ -3,5 +3,9 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("dual_piper", package_name="dual_piper_moviet_3").to_moveit_configs()
+    moveit_config = (
+        MoveItConfigsBuilder("dual_piper", package_name="dual_piper_moviet_3")
+        .ros2_control()
+        .to_moveit_configs()
+    )
     return generate_demo_launch(moveit_config)
